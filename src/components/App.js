23f40c5601fa5b314c9d433f5c55e3861633609a
abhilasha <abhilasha15597@gmail.com>
2022-11-18 14:37:11 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,22 @@ import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here 
-  const [toggle,settoggle] = useState(false);
-  
+  const [btn1,setbtn1] = useState(true);
+  const [btn2,setbtn2] = useState(false);
 
-  const handleClick = () =>{
-    settoggle(!toggle);
+  const handleClick = (e) =>{
+    setbtn1(!btn1);
+    setbtn2(!btn2);
   }
   
 
   
   return (
     <div className="App">
-      <button id="btn1" onClick={handleClick}>{toggle}</button>
+      <button id="btn1" onClick={handleClick}>{(btn1)? "OFF":"ON"}</button>
       <br />
       <br />
-      <button id="btn2" onClick={handleClick}>{toggle}</button>
+      <button id="btn2" onClick={handleClick}>{(btn2)?"ON":"OFF"}</button>
     </div>
   );
 }
