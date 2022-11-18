759f5433812c14cd5884553ba3f80e89495c3a57
abhilasha <abhilasha15597@gmail.com>
2022-11-18 15:47:53 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
   }
   return (
     <div className="App">
-      <button id="btn1" onClick={handleClick}>{(toggle)? "OFF":"ON"}</button>
+      <button id="btn1" onClick={handleClick}>{(toggle)?"OFF":"ON"}</button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>{(toggle)?"ON":"OFF"}</button>
